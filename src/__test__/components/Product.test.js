fc417a550af32d1031e867ca4c3b757a779b79fe
leonardo <clgrhc@gmail.com>
2020-05-08 05:48:10 -0500

    initial commit for travis integration

---
+++ src/__test__/components/Product.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import Product from '../../components/Product';
+import ProductMock from '../../__mocks__/ProductMock';
+import ProviderMock from '../../__mocks__/ProviderMock';
+
+describe('<Product />', () => {
+  test('Render del componente products', () => {
+    const product = shallow(
+      <ProviderMock>
+        <Product />
+      </ProviderMock>,
+    );
+
+    expect(product.length).toEqual(1);
+  });
+  test('Comprobar el botón de comprar', () => {
+    const handleAddToCart = jest.fn();
+    const wrapper = mount(
+      <ProviderMock>
+        <Product product={ProductMock} handleAddToCart={handleAddToCart} />
+      </ProviderMock>,
+    );
+    wrapper.find('button').simulate('click');
+    expect(handleAddToCart).toHaveBeenCalledTimes(1);
+  });
+});
